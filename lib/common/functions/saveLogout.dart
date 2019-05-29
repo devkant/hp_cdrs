aff3a0232763ec40763263e36cdf099435eb55ef
devkant <devkantchouhan@gmail.com>
2019-05-29 06:02:07 +0530

    added logout function

---
+++ lib/common/functions/saveLogout.dart
@@ -0,0 +1,9 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+saveLogout() async {
+  SharedPreferences preferences = await SharedPreferences.getInstance();
+
+  await preferences.setString('LastUser', "");
+  await preferences.setString('LastToken', "");
+  await preferences.setInt('LastUserId', 0);
+}
