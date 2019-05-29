42e5ec9efeb71413eb2871910d11a5c401f7d7ff
devkant <dev.chouhan9213@gmail.com>
2019-05-29 20:16:47 +0530

    added some text

--- lib/app_screens/homeScreen.dart
+++ lib/app_screens/homeScreen.dart
@@ -42,7 +42,7 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Center(
           child: Column(
             children: <Widget>[
-              Text('This is the Home screen'),
+              Text('This is the Home screen-- Login Successful'),
 
             ],
           ),
