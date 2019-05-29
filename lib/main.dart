594c37373d20e0aafb442f2b983d38229a4ccfe6
devkant <dev.chouhan9213@gmail.com>
2019-05-29 17:22:30 +0530

    added navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,86 +1,31 @@
 //change this commited in Login Page
 
 import "package:flutter/material.dart";
+import 'package:hp_cdrs/app_screens/loginPage.dart';
+import 'package:hp_cdrs/app_screens/splashScreen.dart';
+import 'package:hp_cdrs/app_screens/homeScreen.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() =>  runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  var _splashShown = false;
+
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return MaterialApp(
-        title: "HP Govt",
-        home: LoginPage(),
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ));
-  }
-}
-
-class LoginPage extends StatefulWidget {
-  @override
-  State createState() => LoginPageState();
-}
-
-class LoginPageState extends State<LoginPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Child Death Reporting App"),
-        ),
-        backgroundColor: Colors.white,
-        body: new Container(
-            child: new Padding(
-                padding: const EdgeInsets.all(40.0),
-                child: SingleChildScrollView(
-                  child: new Column(
-                    children: <Widget>[
-                      Image(
-                        image: new AssetImage("lib/assets/hpgovet.jpg"),
-
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(20.0),
-                      ),
-
-                      TextFormField(
-                        decoration: InputDecoration(
-                            labelText: "Username",
-                            hintText: "Enter Username",
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(8.0))),
-                        keyboardType: TextInputType.text,
-                      ),
-                      Padding(padding: const EdgeInsets.all(20.0)),
-                      TextFormField(
-                        decoration: new InputDecoration(
-                            labelText: "Password",
-                            hintText: "Enter Password",
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(8.0),
-                            )),
-                        keyboardType: TextInputType.text,
-                        obscureText: true,
-                      ),
-                      Padding(padding: const EdgeInsets.all(40.0)),
-                      RaisedButton(
-                        child: new Text(
-                          "Login",
-                          style: TextStyle(
-                              fontSize: 25.0,
-                              color: Colors.white
-                          ),
-                        ),
-                        onPressed: null,
-                        splashColor: Colors.redAccent,
-                      ),
-                    ],
-                  ),
-                )
-            )
-        )
+      title: "CDRS",
+      routes: <String,WidgetBuilder>{
+        "/HomeScreen":(BuildContext context)  =>  HomeScreen(),
+        "/LoginPage" :(BuildContext context)  =>  LoginPage(),
+      },
+      home: SplashScreen(),
     );
   }
+
+
 }
+
+
+
+
