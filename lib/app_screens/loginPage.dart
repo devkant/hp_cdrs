3f1dfa29b664ac99935ec0d55833b30a00e975cb
devkant <dev.chouhan9213@gmail.com>
2019-05-29 17:07:27 +0530

    added navigation

--- lib/app_screens/loginPage.dart
+++ lib/app_screens/loginPage.dart
@@ -4,89 +4,110 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/services.dart';
 import 'package:hp_cdrs/common/functions/showDialogSingleButton.dart';
 import 'package:hp_cdrs/common/apifunctions/requestLoginAPI.dart';
-
+import 'package:hp_cdrs/common/widgets/basicDrawer.dart';
 import 'package:http/http.dart' as http;
 
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: "HP Govt",
-        home: LoginPage(),
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ));
+  State<StatefulWidget> createState(){
+    return  new LoginPageState();
   }
 }
 
-class LoginPage extends StatefulWidget {
-  @override
-  State createState() => LoginPageState();
-}
-
 class LoginPageState extends State<LoginPage> {
+  final TextEditingController _userNameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  String _welcomeString = "";
+  @override
+  void initstate(){
+    super.initState();
+    _saveCurrentRoute("/LoginPage");
+  }
+
+  _saveCurrentRoute(String lastRoute) async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    await preferences.setString('LastScreenRoute', lastRoute);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Child Death Reporting App"),
-        ),
-        backgroundColor: Colors.white,
-        body: new Container(
-            child: new Padding(
-                padding: const EdgeInsets.all(40.0),
-                child: SingleChildScrollView(
-                  child: new Column(
-                    children: <Widget>[
-                      Image(
-                        image: new AssetImage("assets/hpgovet.jpg"),
+    var drawer  = Drawer();
+    return WillPopScope(
+      onWillPop: () {
+        if(Navigator.canPop(context)){
+          Navigator.of(context).pushNamedAndRemoveUntil('\HomeScreen',(Route<dynamic> route)  =>false);
+        }
+        else{
+          Navigator.of(context).pushReplacementNamed('\HomeScreen');
+        }
+      },
+      child: Scaffold(
+          drawer: BasicDrawer(),
+          appBar: AppBar(
+            title: Text("Child Death Reporting App"),
+          ),
+          backgroundColor: Colors.white,
+          body: new Container(
+              child: new Padding(
+                  padding: const EdgeInsets.all(40.0),
+                  child: SingleChildScrollView(
+                    child: new Column(
+                      children: <Widget>[
+                        Image(
+                          image: new AssetImage("assets/hpgovet.jpg"),
 
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(20.0),
-                      ),
-
-                      TextFormField(
-                        decoration: InputDecoration(
-                            labelText: "Username",
-                            hintText: "Enter Username",
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(8.0))),
-                        keyboardType: TextInputType.text,
-                      ),
-                      Padding(padding: const EdgeInsets.all(20.0)),
-                      TextFormField(
-                        decoration: new InputDecoration(
-                            labelText: "Password",
-                            hintText: "Enter Password",
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(8.0),
-                            )),
-                        keyboardType: TextInputType.text,
-                        obscureText: true,
-                      ),
-                      Padding(padding: const EdgeInsets.all(40.0)),
-                      RaisedButton(
-                        child: new Text(
-                          "Login",
-                          style: TextStyle(
-                              fontSize: 25.0,
-                              color: Colors.white
-                          ),
                         ),
-                        onPressed: null,
-                        splashColor: Colors.redAccent,
-                      ),
-                    ],
-                  ),
-                )
-            )
-        )
+                        Padding(
+                          padding: const EdgeInsets.all(20.0),
+                        ),
+
+                        TextFormField(
+                          controller: _userNameController,
+                          decoration: InputDecoration(
+                              labelText: "Username",
+                              hintText: "Enter Username",
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(8.0))),
+                          keyboardType: TextInputType.text,
+                        ),
+                        Padding(padding: const EdgeInsets.all(20.0)),
+                        TextFormField(
+                          controller: _passwordController,
+                          decoration: new InputDecoration(
+                              labelText: "Password",
+                              hintText: "Enter Password",
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8.0),
+                              )),
+                          keyboardType: TextInputType.text,
+                          obscureText: true,
+                        ),
+                        Padding(padding: const EdgeInsets.all(40.0)),
+                        RaisedButton(
+                          child: new Text(
+                            "Login",
+                            style: TextStyle(
+                                fontSize: 25.0,
+                                color: Colors.white
+                            ),
+                          ),
+                          onPressed: (){
+                            SystemChannels.textInput.invokeMethod('TextInput.hide');
+                            requestLoginAPI(context, _userNameController.text, _passwordController.text);
+                          },
+                          splashColor: Colors.blue,
+                        ),
+                      ],
+                    ),
+                  )
+              )
+          )
+      ),
     );
+
+
+
+
   }
 }
