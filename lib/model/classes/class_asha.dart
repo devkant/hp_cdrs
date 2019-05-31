a6fb50c6a184d3c985f84a52212d271def650bc8
devkant <dev.chouhan9213@gmail.com>
2019-05-31 21:05:53 +0530

    Updates

--- lib/model/classes/class_asha.dart
+++ lib/model/classes/class_asha.dart
@@ -1,24 +1,34 @@
 
 class Child {
-  String  name,district,block,address,phoneNumber;
+  final String  name;
+  final String  district;
+  final String  block;
+  final String  address;
+  final String  phoneNumber;
 
-  Child({
+  Child(
     this.name,
     this.district,
     this.block,
     this.address,
     this.phoneNumber,
-  });
+  );
 
-  factory Child.fromJson(Map<String,dynamic> json){
-    return  new Child(
-      name: json['name'],
-      district: json['district'],
-      block: json['block'],
-      address: json['address'],
-      phoneNumber: json['phoneNumber']
+  Child.fromJson(Map<String,dynamic> json)
+      : name  = json['name'],
+        district  = json['district'],
+        block = json['block'],
+        address = json['address'],
+        phoneNumber = json['phoneNumber'];
 
-    );
 
-  }
+
+  Map<String, dynamic>  toJson()  =>{
+    'name': name,
+    'district': district,
+    'block' : block,
+    'address' : address,
+    'phoneNumber':  phoneNumber,
+  };
+
 }
