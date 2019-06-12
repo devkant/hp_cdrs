b8f4a7d1d38e111f8978963782663f59ff06b32b
devkant <dev.chouhan9213@gmail.com>
2019-06-12 17:26:43 +0530

    Added Future

--- lib/common/functions/getToken.dart
+++ lib/common/functions/getToken.dart
@@ -1,6 +1,6 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
-getToken() async {
+Future<String>getToken() async {
   SharedPreferences preferences = await SharedPreferences.getInstance();
 
   String getToken = await preferences.getString("LastToken");
