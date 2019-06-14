3a6a21e30b17265d34493bbaa5926b941d645eb8
devkant <dev.chouhan9213@gmail.com>
2019-06-14 06:28:59 +0530

    Added Connection Status Singelton

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import "package:flutter/material.dart";
 import 'package:hp_cdrs/app_screens/loginPage.dart';
 import 'package:hp_cdrs/app_screens/splashScreen.dart';
 import 'package:hp_cdrs/app_screens/homeScreen.dart';
+import 'package:hp_cdrs/connectionStatus.dart';
 
 /*void main() =>  runApp(MyApp());
 
@@ -25,7 +26,11 @@ class MyApp extends StatelessWidget {
 
 }*/
 
+
 Future<void> main() async {
+  ConnectionStatusSingleton connectionStatus = ConnectionStatusSingleton.getInstance();
+  connectionStatus.initialize();
+
   SharedPreferences prefs = await SharedPreferences.getInstance();
   var name = prefs.getString('LastUser');
   var role  = prefs.getString('LastUserId');
