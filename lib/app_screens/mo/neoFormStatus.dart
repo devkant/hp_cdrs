31b925dbc43cea0dae6db82c4d209bbebf06007b
devkant <dev.chouhan9213@gmail.com>
2019-06-15 23:49:41 +0530

    Renamed File

--- lib/app_screens/mo/neoFormStatus.dart
+++ lib/app_screens/mo/neoFormStatus.dart
@@ -3,6 +3,7 @@ import 'package:hp_cdrs/connectionStatus.dart';
 import 'dart:async';
 import 'dart:io';
 import 'dart:convert';
+import 'package:hp_cdrs/app_screens/verbal_autopsy/user.dart';
 
 class FormsStatus extends StatefulWidget {
   @override
@@ -39,7 +40,7 @@ class _FormsStatusState extends State<FormsStatus> {
     });
   }
 
-  void createFile(Child content, Directory dir, String fileName) {
+  void createFile(user content, Directory dir, String fileName) {
     print("Creating file!");
     File file = new File(dir.path + "/" + fileName);
     file.createSync();
@@ -47,7 +48,7 @@ class _FormsStatusState extends State<FormsStatus> {
     file.writeAsStringSync(json.encode(content),mode: FileMode.append);
   }
 
-  void writeToFile(Child entry) {
+  void writeToFile(user entry) {
     print("Writing to file!");
     if (fileExists) {
       print("File exists");
