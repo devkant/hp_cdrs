8ebac140d9b4bf9adafbc68b06d59f7a9f962210
devkant <dev.chouhan9213@gmail.com>
2019-06-17 21:43:47 +0530

    Fixed

--- lib/app_screens/loginPage.dart
+++ lib/app_screens/loginPage.dart
@@ -9,13 +9,8 @@ class LoginPage extends StatefulWidget {
 }
 
 class LoginPageState extends State<LoginPage> {
-
-  final _formKey = GlobalKey<FormState>();
-  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
-
   final TextEditingController _userNameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-  final TextEditingController _email = TextEditingController();
   @override
   void initstate(){
     super.initState();
@@ -41,179 +36,116 @@ class LoginPageState extends State<LoginPage> {
         }
       },
       child: Scaffold(
-        key: _scaffoldKey,
           appBar: AppBar(
             title: Text("Child Death Reporting App"),
           ),
           backgroundColor: Colors.white,
-          body:  Form(
-            key: _formKey,
-            child: Container(
-                child:  Padding(
-                    padding: const EdgeInsets.all(40.0),
-                    child: SingleChildScrollView(
-                      child: Column(
-                        children: <Widget>[
-                          CircleAvatar(
-                            radius:60.0,
-                            backgroundColor: Colors.white,
-                            child: Image(
-                              image: AssetImage('assets/hpgovt.png'),
-                              height: 120.0,
-                            ),
+          body:  Container(
+              child:  Padding(
+                  padding: const EdgeInsets.all(40.0),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: <Widget>[
+                        CircleAvatar(
+                          radius:60.0,
+                          backgroundColor: Colors.white,
+                          child: Image(
+                            image: AssetImage('assets/hpgovt.png'),
+                            height: 120.0,
                           ),
-                          Padding(
-                            padding: const EdgeInsets.all(10.0),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(10.0),
+                        ),
+                        Text(
+                          'LOGIN HERE',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.w400
                           ),
-                          Text(
-                            'LOGIN HERE',
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 20.0,
-                                fontWeight: FontWeight.w400
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(15.0),
-                          ),
-                          TextFormField(
-                            validator: (value)  {
-                              if (value.length == 0 || double.parse(value) == 0.0) {
-                                return ('Please enter a valid input');
-                              }
-                            },
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(15.0),
+                        ),
+                        TextFormField(
+                          validator: (value)  {
+                            if (value.length == 0 || double.parse(value) == 0.0) {
+                              return ('Enter valid');
+                            }
+                          },
 
-                            controller: _userNameController,
-                            style: TextStyle(fontSize: 15.0),
-                            decoration: InputDecoration(
-                                border: UnderlineInputBorder(
-                                    borderRadius: BorderRadius.horizontal(
-                                        left: Radius.circular(20.0),
-                                        right: Radius.circular(20.0)
-                                    ),
-                                    borderSide: BorderSide(width: 20.0, color: Colors.blue)
-                                ),
-                                labelText: 'Username',
-                                hintText: 'Enter Username',
-                                contentPadding: EdgeInsets.only(left: 20.0,bottom: 10.0)
-                            ),
-                            keyboardType: TextInputType.text,
-                          ),
-                          Padding(padding: const EdgeInsets.all(20.0)),
-                          TextFormField(
-                            validator:(String value) {
-                              if(value.isEmpty) {
-                                return 'Please enter a valid input';
-                              }
-                            },
-                            controller: _passwordController,
-                            keyboardType: TextInputType.text,
-                            obscureText: true,
-                            style: TextStyle(fontSize: 15.0),
-                            decoration: InputDecoration(
-                                border: UnderlineInputBorder(
-                                    borderRadius: BorderRadius.horizontal(
-                                        left: Radius.circular(20.0),
-                                        right: Radius.circular(20.0)
-                                    ),
-                                    borderSide: BorderSide(width: 20.0, color: Colors.blue)
-                                ),
-                                labelText: 'Password',
-                                hintText: 'Enter Password',
-                                contentPadding: EdgeInsets.only(left: 20.0,bottom: 10.0)
-                            ),
-                          ),
-                          Padding(padding: const EdgeInsets.all(20.0)),
-                          RaisedButton(
-                            color: Colors.blue,
-                            child: new Text(
-                              "Login",
-                              style: TextStyle(
-                                  fontSize: 25.0,
-                                  color: Colors.white
-                              ),
-                            ),
-                            onPressed: (){
-                              if(_formKey.currentState.validate()) {
-                                SystemChannels.textInput.invokeMethod('TextInput.hide');
-                                requestLoginAPI(context, _userNameController.text,_passwordController.text);
-                              }
-                            },
-                            splashColor: Colors.blue,
-                          ),
-                          Padding(
-                              padding: EdgeInsets.all(20.0),
-                              child: FlatButton(
-                                onPressed: (){showAlertFun();},
-                                child: Text(
-                                  'Forgot Password',
-                                  style: TextStyle(
-                                    decoration: TextDecoration.underline,
-                                    color: Colors.blue,
+                          controller: _userNameController,
+                          style: TextStyle(fontSize: 15.0),
+                          decoration: InputDecoration(
+                              border: UnderlineInputBorder(
+                                  borderRadius: BorderRadius.horizontal(
+                                      left: Radius.circular(20.0),
+                                      right: Radius.circular(20.0)
                                   ),
-
+                                  borderSide: BorderSide(width: 20.0, color: Colors.blue)
+                              ),
+                              labelText: 'Username',
+                              hintText: 'Enter Username',
+                              contentPadding: EdgeInsets.only(left: 20.0,bottom: 10.0)
+                          ),
+                          keyboardType: TextInputType.text,
+                        ),
+                        Padding(padding: const EdgeInsets.all(20.0)),
+                        TextFormField(
+                          controller: _passwordController,
+                          keyboardType: TextInputType.text,
+                          obscureText: true,
+                          style: TextStyle(fontSize: 15.0),
+                          decoration: InputDecoration(
+                              border: UnderlineInputBorder(
+                                  borderRadius: BorderRadius.horizontal(
+                                      left: Radius.circular(20.0),
+                                      right: Radius.circular(20.0)
+                                  ),
+                                  borderSide: BorderSide(width: 20.0, color: Colors.blue)
+                              ),
+                              labelText: 'Password',
+                              hintText: 'Enter Password',
+                              contentPadding: EdgeInsets.only(left: 20.0,bottom: 10.0)
+                          ),
+                        ),
+                        Padding(padding: const EdgeInsets.all(20.0)),
+                        RaisedButton(
+                          color: Colors.blue,
+                          child: new Text(
+                            "Login",
+                            style: TextStyle(
+                                fontSize: 25.0,
+                                color: Colors.white
+                            ),
+                          ),
+                          onPressed: (){
+                            SystemChannels.textInput.invokeMethod('TextInput.hide');
+                            requestLoginAPI(context, _userNameController.text, _passwordController.text);
+                          },
+                          splashColor: Colors.blue,
+                        ),
+                        Padding(
+                            padding: EdgeInsets.all(20.0),
+                            child: FlatButton(
+                              child: Text(
+                                'Forgot Password',
+                                style: TextStyle(
+                                  decoration: TextDecoration.underline,
+                                  color: Colors.blue,
                                 ),
 
-                              )
-                          )
-                        ],
-                      ),
-                    )
-                )
-            ),
+                              ),
+
+                            )
+                        )
+                      ],
+                    ),
+                  )
+              )
           )
       ),
-    );
-  }
-
-  void emailSendFun(){
-    Navigator.pop(context);
-  }
-
-
-  void showAlertFun() {
-    AlertDialog dialog = AlertDialog(
-      content: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.all(10.0),
-              child: Text('Please Enter your E-mail below:'),),
-
-            Padding(
-                padding: EdgeInsets.all(10.0),
-                child: TextFormField(
-                    controller: _email,
-                    keyboardType: TextInputType.emailAddress,
-                    validator: (String value) {
-                      if (value.isEmpty) return 'Please input the E-mail';
-                    },
-                    decoration: InputDecoration(
-                        labelText: 'E-mail',
-                        hintText: 'abcdef@gmail.com',
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10.0))))),
-
-            RaisedButton(
-                color: Colors.blue,
-
-                child: Text('Send',
-                  style: TextStyle(color: Colors.white),),
-                onPressed: () {
-                  emailSendFun();
-                })
-          ],
-        ),
-        width: 150.0,
-        height: 200.0,
-      ),
-    );
-
-    showDialog(context: context,
-        builder: (BuildContext context) {
-          return dialog;
-        }
     );
   }
 
