12bcfd63bb80f2ffa856b7247e794c9f963fc187
Amol11 <amolsinghaniya@gmail.com>
2019-06-17 15:08:53 +0530

    fixed the login page errors after merge conflict

--- lib/app_screens/loginPage.dart
+++ lib/app_screens/loginPage.dart
@@ -121,25 +121,11 @@ class LoginPageState extends State<LoginPage> {
                                 contentPadding: EdgeInsets.only(left: 20.0,bottom: 10.0)
                             ),
                           ),
-
-                          onPressed: (){
-                            SystemChannels.textInput.invokeMethod('TextInput.hide');
-                            requestLoginAPI(context, _userNameController.text, _passwordController.text);
-                          },
-                          splashColor: Colors.blue,
-                        ),
-                        Padding(
-                          padding: EdgeInsets.all(20.0),
-                          child: FlatButton(
-                            onPressed: (){showAlertFun();},
-                            child: Text(
-                              'Forgot Password',
                           Padding(padding: const EdgeInsets.all(20.0)),
                           RaisedButton(
                             color: Colors.blue,
                             child: new Text(
                               "Login",
-
                               style: TextStyle(
                                   fontSize: 25.0,
                                   color: Colors.white
@@ -156,6 +142,7 @@ class LoginPageState extends State<LoginPage> {
                           Padding(
                               padding: EdgeInsets.all(20.0),
                               child: FlatButton(
+                                onPressed: (){showAlertFun();},
                                 child: Text(
                                   'Forgot Password',
                                   style: TextStyle(
@@ -186,44 +173,47 @@ class LoginPageState extends State<LoginPage> {
 
     AlertDialog dialog = AlertDialog(
       content: Container(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-        Padding(
-          padding: EdgeInsets.all(10.0),
-        child: Text('Please Enter your E-mail below:'),),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Text('Please Enter your E-mail below:'),),
 
-        Padding(
-          padding: EdgeInsets.all(10.0),
-        child: TextFormField(
-            controller: _email,
-            keyboardType: TextInputType.emailAddress,
-            validator: (String value) {
-              if (value.isEmpty) return 'Please input the E-mail';
-            },
-            decoration: InputDecoration(
-                labelText: 'E-mail',
-                hintText: 'abcdef@gmail.com',
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0))))),
+            Padding(
+                padding: EdgeInsets.all(10.0),
+                child: TextFormField(
+                    controller: _email,
+                    keyboardType: TextInputType.emailAddress,
+                    validator: (String value) {
+                      if (value.isEmpty) return 'Please input the E-mail';
+                    },
+                    decoration: InputDecoration(
+                        labelText: 'E-mail',
+                        hintText: 'abcdef@gmail.com',
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0))))),
 
-        RaisedButton(
-          color: Colors.blue,
+            RaisedButton(
+                color: Colors.blue,
 
-            child: Text('Send',
-              style: TextStyle(color: Colors.white),),
-            onPressed: (){emailSendFun();})
-      ],
+                child: Text('Send',
+                  style: TextStyle(color: Colors.white),),
+                onPressed: (){emailSendFun();})
+          ],
         ),
-      width: 150.0,
+        width: 150.0,
         height: 200.0,
       ),
     );
 
-    showDialog(context: context, child: dialog);
+    showDialog(context: context,
+        builder: (BuildContext context){
+          return dialog;
+        }
+    );
 
 
 
 
-  }
 }
