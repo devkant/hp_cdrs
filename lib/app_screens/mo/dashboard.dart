d3de197597d33a508ae1a77f6fd8b2a50048901a
Amol11 <amolsinghaniya@gmail.com>
2019-06-21 02:29:56 +0530

    fixed back button issue

--- lib/app_screens/mo/dashboard.dart
+++ lib/app_screens/mo/dashboard.dart
@@ -24,136 +24,142 @@ class Dashboard extends StatefulWidget {
 
 class _DashboardState extends State<Dashboard> {
 
+  Future<bool> onBackPress() async{
+    return false;
+  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: BasicDrawer(),
-      appBar: AppBar(
-        title: Text("MO DashBoard"),
-        elevation: .1,
-        backgroundColor: Colors.blue,
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 2.0),
-        child: GridView.count(
-          crossAxisCount: 2,
-          padding: EdgeInsets.all(3.0),
-          children: <Widget>[
-            Card(
-                elevation: 1.0,
-                margin: new EdgeInsets.all(8.0),
-                child: Container(
-                  decoration: BoxDecoration(color: Color.fromRGBO(220, 220, 220, 1.0)),
-                  child: new InkWell(
-                    onTap: () {
-                      Navigator.of(context).push(
-                          MaterialPageRoute(
-                              builder: (BuildContext  context)  =>  neoFormsStatus())
-                      );
+    return WillPopScope(
+      onWillPop: onBackPress,
+      child: Scaffold(
+        drawer: BasicDrawer(),
+        appBar: AppBar(
+          title: Text("MO DashBoard"),
+          elevation: .1,
+          backgroundColor: Colors.blue,
+        ),
+        body: Container(
+          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 2.0),
+          child: GridView.count(
+            crossAxisCount: 2,
+            padding: EdgeInsets.all(3.0),
+            children: <Widget>[
+              Card(
+                  elevation: 1.0,
+                  margin: new EdgeInsets.all(8.0),
+                  child: Container(
+                    decoration: BoxDecoration(color: Color.fromRGBO(220, 220, 220, 1.0)),
+                    child: new InkWell(
+                      onTap: () {
+                        Navigator.of(context).push(
+                            MaterialPageRoute(
+                                builder: (BuildContext  context)  =>  neoFormsStatus())
+                        );
 
-                    },
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      mainAxisSize: MainAxisSize.min,
-                      verticalDirection: VerticalDirection.down,
-                      children: <Widget>[
-                        SizedBox(height: 50.0),
-                        Center(
-                            child: Icon(
-                              Icons.child_friendly,
-                              size: 40.0,
-                              color: Colors.black,
-                            )),
-                        SizedBox(height: 20.0),
-                        new Center(
-                          child: new Text('Verbal Autopsy\n (Neonate)',
-                              style:
-                              new TextStyle(fontSize: 15.0, color: Colors.black)),
-                        )
-                      ],
+                      },
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        mainAxisSize: MainAxisSize.min,
+                        verticalDirection: VerticalDirection.down,
+                        children: <Widget>[
+                          SizedBox(height: 50.0),
+                          Center(
+                              child: Icon(
+                                Icons.child_friendly,
+                                size: 40.0,
+                                color: Colors.black,
+                              )),
+                          SizedBox(height: 20.0),
+                          new Center(
+                            child: new Text('Verbal Autopsy\n (Neonate)',
+                                style:
+                                new TextStyle(fontSize: 15.0, color: Colors.black)),
+                          )
+                        ],
+                      ),
                     ),
                   ),
-                ),
-            ),
-            Card(
-                elevation: 1.0,
-                margin: new EdgeInsets.all(8.0),
-                child: Container(
-                  decoration: BoxDecoration(color: Color.fromRGBO(220, 220, 220, 1.0)),
-                  child: new InkWell(
-                    onTap: () {
-                      Navigator.of(context).push(
-                          MaterialPageRoute(
-                              builder: (BuildContext  context)  =>  SocialAutopsyFormStatus())
-                      );
+              ),
+              Card(
+                  elevation: 1.0,
+                  margin: new EdgeInsets.all(8.0),
+                  child: Container(
+                    decoration: BoxDecoration(color: Color.fromRGBO(220, 220, 220, 1.0)),
+                    child: new InkWell(
+                      onTap: () {
+                        Navigator.of(context).push(
+                            MaterialPageRoute(
+                                builder: (BuildContext  context)  =>  SocialAutopsyFormStatus())
+                        );
 
-                    },
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      mainAxisSize: MainAxisSize.min,
-                      verticalDirection: VerticalDirection.down,
-                      children: <Widget>[
-                        SizedBox(height: 50.0),
-                        Center(
-                            child: Icon(
-                              Icons.people,
-                              size: 40.0,
-                              color: Colors.black,
-                            )),
-                        SizedBox(height: 20.0),
-                        new Center(
-                          child: new Text('Social Autopsy',
-                              style:
-                              new TextStyle(fontSize: 15.0, color: Colors.black)),
-                        )
-                      ],
+                      },
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        mainAxisSize: MainAxisSize.min,
+                        verticalDirection: VerticalDirection.down,
+                        children: <Widget>[
+                          SizedBox(height: 50.0),
+                          Center(
+                              child: Icon(
+                                Icons.people,
+                                size: 40.0,
+                                color: Colors.black,
+                              )),
+                          SizedBox(height: 20.0),
+                          new Center(
+                            child: new Text('Social Autopsy',
+                                style:
+                                new TextStyle(fontSize: 15.0, color: Colors.black)),
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                )
+                  )
 
-            )
-            ,
-            Card(
-                elevation: 1.0,
-                margin: new EdgeInsets.all(8.0),
-                child: Container(
-                  decoration: BoxDecoration(color: Color.fromRGBO(220, 220, 220, 1.0)),
-                  child: new InkWell(
-                    onTap: () {
-                      Navigator.of(context).push(
-                          MaterialPageRoute(
-                              builder: (BuildContext  context)  =>  PostNeoFormsStatus())
-                      );
+              )
+              ,
+              Card(
+                  elevation: 1.0,
+                  margin: new EdgeInsets.all(8.0),
+                  child: Container(
+                    decoration: BoxDecoration(color: Color.fromRGBO(220, 220, 220, 1.0)),
+                    child: new InkWell(
+                      onTap: () {
+                        Navigator.of(context).push(
+                            MaterialPageRoute(
+                                builder: (BuildContext  context)  =>  PostNeoFormsStatus())
+                        );
 
-                    },
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      mainAxisSize: MainAxisSize.min,
-                      verticalDirection: VerticalDirection.down,
-                      children: <Widget>[
-                        SizedBox(height: 50.0),
-                        Center(
-                            child: Icon(
-                              Icons.child_care,
-                              size: 40.0,
-                              color: Colors.black,
-                            )),
-                        SizedBox(height: 20.0),
-                        new Center(
-                          child: new Text('Verbal Autopsy\n  (Post Neonate)',
-                              style:
-                              new TextStyle(fontSize: 15.0, color: Colors.black)),
-                        )
-                      ],
+                      },
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        mainAxisSize: MainAxisSize.min,
+                        verticalDirection: VerticalDirection.down,
+                        children: <Widget>[
+                          SizedBox(height: 50.0),
+                          Center(
+                              child: Icon(
+                                Icons.child_care,
+                                size: 40.0,
+                                color: Colors.black,
+                              )),
+                          SizedBox(height: 20.0),
+                          new Center(
+                            child: new Text('Verbal Autopsy\n  (Post Neonate)',
+                                style:
+                                new TextStyle(fontSize: 15.0, color: Colors.black)),
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                )
+                  )
 
 
-            )
+              )
 
 
-          ],
+            ],
+          ),
         ),
       ),
     );
