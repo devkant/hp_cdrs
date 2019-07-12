a53a8ea73d4cc4290ed41252756c2408d0440379
devkant <dev.chouhan9213@gmail.com>
2019-07-12 17:27:46 +0530

    Working

--- lib/app_screens/mo/previousForm.dart
+++ lib/app_screens/mo/previousForm.dart
@@ -19,21 +19,19 @@ class MyApp extends StatelessWidget {
 class Appli{
   String  appliNumber;
   String  name;
-  String  address;
   String  neoFilled;
   String  postNeoFilled;
   String  socialFilled;
 
-  Appli({this.appliNumber,this.name,this.address,this.neoFilled,this.postNeoFilled,this.socialFilled});
+  Appli({this.appliNumber,this.name,this.neoFilled,this.postNeoFilled,this.socialFilled});
 
   factory Appli.fromJson(Map<String,dynamic> parsedJson){
     return  Appli(
-      appliNumber:  parsedJson['appliNumber'],
+      appliNumber:  parsedJson['application'],
       name: parsedJson['name'],
-      address:  parsedJson['address'],
-      neoFilled:  parsedJson['neoFilled'],
-      postNeoFilled:  parsedJson['postNeoFilled'],
-      socialFilled: parsedJson['socialFilled']
+      neoFilled:  parsedJson['neonate'],
+      postNeoFilled:  parsedJson['postNeonate'],
+      socialFilled: parsedJson['social']
 
     );
   }
@@ -41,11 +39,13 @@ class Appli{
 
 Future<List<Appli>> loadAppli() async{
   final token = await getToken();
-  var response = await http.get('http://13.235.43.83/api/test',
+  var response = await http.get('http://13.235.43.83/api/moPreviousForms',
       headers: {
         'authToken': token,
       }
   );
+  print(response.statusCode);
+  print(response.body);
   if(response.statusCode==200){
     final items = json.decode(response.body).cast<Map<String,dynamic>>();
     List  <Appli> listOfAppli = items.map<Appli>((json) {
@@ -80,28 +80,160 @@ class PreviousFormState extends State<PreviousForm> {
       body: FutureBuilder<List<Appli>>(
         future: loadAppli(),
         builder: (context, snapshot) {
+          print(snapshot);
           if (!snapshot.hasData)
             return Center(child: CircularProgressIndicator());
+          if(snapshot.data.isEmpty)
+            return Center(
+                child: Text(
+                    'No Forms Filled',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.black,
+                    ),
+                ),
 
-          return ListView(
-            children: snapshot.data
-                .map((user) =>
-                ListTile(
-                  title: Text(user.name),
-                  subtitle: Text(user.address),
-                  leading: CircleAvatar(
-                    backgroundColor: Colors.red,
-                    child: Text(user.name[0],
-                        style: TextStyle(
-                          fontSize: 18.0,
-                          color: Colors.white,
-                        )),
-                  ),
-                  onTap: () {},
-                )).toList(),
-          );
+            );
+          else {
+            return ListView(
+              children: snapshot.data
+                  .map((user) =>
+                  InkWell(
+                    child: Card(
+                        margin: new EdgeInsets.all(10.0),
+                        elevation: 20.0,
+                        child: Row(
+                        children: <Widget>[
+                          Container(
+                            margin: EdgeInsets.fromLTRB(10.0, 40.0, 40.0, 40.0),
+                            child: Column(
+                              children: <Widget>[
+                                Text(user.appliNumber,style:TextStyle(fontSize: 18.0,fontWeight: FontWeight.bold),),
+                                Text('Name: ${user.name}',style:TextStyle(fontSize: 18.0,fontWeight: FontWeight.bold)),
+                              ],
+                            ),
+                          ),
+                          Wrap(
+                            children: <Widget>[
+                              Column(
+                                children: <Widget>[
+                                  Padding(
+                                    padding: EdgeInsets.all(8.0),
+                                    child: InkWell(
+                                      child: Text('Neonate: ${user.neoFilled}',
+                                        style: TextStyle(fontSize: 18.0,decoration: TextDecoration.underline),
+
+                                      ),
+                                      onTap: (){},
+
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.all(8.0),
+                                    child: InkWell(
+                                      child: Text('Postneonate: ${user.postNeoFilled}',
+                                        style: TextStyle(fontSize: 18.0,decoration: TextDecoration.underline),
+
+                                      ),
+                                      onTap: (){},
+
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.all(8.0),
+                                    child: InkWell(
+                                      child: Text('Social: ${user.socialFilled}',
+                                        style: TextStyle(fontSize: 18.0,decoration: TextDecoration.underline),
+
+                                      ),
+                                      onTap: (){},
+
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+
+                        ],
+                      )
+                    )
+
+                  )).toList(),
+            );
+          }
         },
       ),
+    );
+  }
+}
+
+
+class Show extends StatefulWidget {
+  String  appliNumber;
+  Show(this.appliNumber);
+  @override
+  ShowState createState() => ShowState();
+}
+
+class ShowState extends State<Show> {
+  final String url  = 'http://13.235.43.83/api/test';
+
+  Future<List<dynamic>>  getData() async {
+    final token = await getToken();
+    var response = await http.get(url,
+        headers: {
+          'authToken' : token,
+          'applicationNumber': widget.appliNumber
+        }
+    );
+    print(response.body);
+    if (response.statusCode == 200) {
+      Map map = json.decode(response.body);
+      final list = map.values.toList(growable: true);
+      print(list);
+      return list;
+    } else {
+      throw Exception('Failed to load internet');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Details'),),
+      body: FutureBuilder<List<dynamic>>(
+        future: getData(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return new Text('loading...');
+            default:
+              if (snapshot.hasError)
+                return new Text('Error: ${snapshot.error}');
+              else
+                return createListView(context, snapshot);
+          }
+        },
+      ),
+    );
+  }
+
+  Widget createListView(BuildContext context, AsyncSnapshot snapshot) {
+    List<dynamic> values = snapshot.data;
+    return new ListView.builder(
+      itemCount: values.length,
+      itemBuilder: (BuildContext context, int index) {
+        return new Column(
+          children: <Widget>[
+            new ListTile(
+              title: new Text(values[index]),
+            ),
+            new Divider(height: 2.0,),
+          ],
+        );
+      },
     );
   }
 }
